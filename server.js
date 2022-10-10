1a289c0436b509f3624b2e842ad4a391e7f4ded2
codigocaballer <josemariacaballerm@gmail.com>
2022-10-10 23:10:40 +0200

    feat: set view engine and index

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ const app = express();
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'pug');
+
 
 app.route('/').get((req, res) => {
-
+    res.render(process.cwd() + "/views/pug");
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+    console.log('Listening on port ' + PORT);
 });
