21b353415a3982a4b1ca0e5951e561ca22274f51
codigocaballer <josemariacaballerm@gmail.com>
2022-10-11 10:28:10 +0200

    feat: enable passport and session

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Advanced Node and Express
 
 This is the boilerplate for the Advanced Node and Express lessons. Instructions for the lessons start at https://www.freecodecamp.org/learn/quality-assurance/advanced-node-and-express/
+
+Running version at https://boilerplate-fcc-advancednode.herokuapp.com/
--- server.js
+++ server.js
@@ -4,12 +4,29 @@ const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const pug = require('pug');
+const {query} = require("mongodb/lib/core/wireprotocol");
 const app = express();
+const passport = require('passport');
+const session = require('express-session');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(passport.initialize());
+
+/*
+connect.sid=s:YR2Xpbe1IwWGbgONcjuOqSw9Wz6m7hyf.xlQUcPEtEFdzQqLV8jeruPMnm0Hx/DN9geHnSIhDVf4; Path=/; HttpOnly
+Content-Length: 534
+Content-Type: text/html; charset=utf-8
+*/
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {secure: false}
+}));
 app.set('view engine', 'pug');
 
 
