837549f190403d2c1d6b5dbd5571557adcf00a78
codigocaballer <josemariacaballerm@gmail.com>
2022-10-18 10:14:01 +0200

    fix: first ensure db connectivity

--- server.js
+++ server.js
@@ -9,13 +9,18 @@ const app = express();
 const passport = require('passport');
 const session = require('express-session');
 const ObjectID = require('mongodb').ObjectID;
+const morgan = require('morgan');
+
 
 fccTesting(app); //For FCC testing purposes
+app.use(morgan('combined'));
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.set('view engine', 'pug');
 
 /*
 connect.sid=s:YR2Xpbe1IwWGbgONcjuOqSw9Wz6m7hyf.xlQUcPEtEFdzQqLV8jeruPMnm0Hx/DN9geHnSIhDVf4; Path=/; HttpOnly
@@ -30,24 +35,26 @@ Content-Type: text/html; charset=utf-8
 //     cookie: {secure: false}
 // }));
 
+myDB(async client => {
+
+    app.route('/').get((req, res) => {
+        res.render(process.cwd() + "/views/pug/index", {title: 'Hello', message: 'Please login'});
+    });
+
 //persistent login session
-passport.serializeUser((user, done) => {
-    done(null, user._id);
-});
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
 
-passport.deserializeUser((id, done) => {
-    // myDB.findOne({_id: new ObjectID(id)}, (err, doc) => {
-    //     done(null, null);
-    // });
-    done(null, null);
-});
-
-
-app.set('view engine', 'pug');
-
-
-app.route('/').get((req, res) => {
-    res.render(process.cwd() + "/views/pug/index", {title: 'Hello', message: 'Please login'});
+    passport.deserializeUser((id, done) => {
+        myDB.findOne({_id: new ObjectID(id)}, (err, doc) => {
+            done(null, doc);
+        });
+    });
+}).catch(e => {
+    app.route('/').get((req, res) => {
+        res.render(process.cwd() + "/views/pug/index", {title: e, message: 'Unable to login'});
+    });
 });
 
 const PORT = process.env.PORT || 3000;
